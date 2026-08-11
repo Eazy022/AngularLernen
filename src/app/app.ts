a42a4f0447982d9f4c2e0db03cff3c1e574e39cf
Emanuel Friedrich (BESITEC-DEHAM) <emanuel.friedrich@besitec.com>
2026-08-11 12:18:27 +0200

    implemented Angular Services

--- src/app/app.ts
+++ src/app/app.ts
@@ -4,7 +4,7 @@ import { Home } from './home/home';
 @Component({
   selector: 'app-root',
   imports: [Home],
-  template: ` <h1>Let´s get started</h1><main>
+  template: ` <h1>It is going Forward!!</h1><main>
        <main>
       <header class="brand-name">
         <img class="brand-logo" src="/public/logo.svg" alt="logo" aria-hidden="true" />

7df6b6ee278e2dffc2261837831ba4d6d47d5071
Emanuel Friedrich (BESITEC-DEHAM) <emanuel.friedrich@besitec.com>
2026-08-11 13:24:25 +0200

    made route parameters

--- src/app/routes.ts
+++ src/app/routes.ts
@@ -1,6 +1,6 @@
 import { Routes } from "@angular/router";
 import {Home} from './home/home'
-import { Details} from "./details/details";
+import { DetailsComponent } from "./details/details";
 
 const routeConfig: Routes = [
      {
@@ -10,7 +10,7 @@ const routeConfig: Routes = [
   },
   {
     path: 'details/:id',
-    component: Details,
+    component: DetailsComponent,
     title: 'Home details',
   },
 ];

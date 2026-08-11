e6a435ddd2bebab26a97bcf32ce84f03fa88f2aa
Eazy022 <emanuel_friedrich@web.de>
2026-08-11 16:37:11 +0200

    implemented filter function

--- src/app/home/home.ts
+++ src/app/home/home.ts
@@ -8,24 +8,42 @@ import {HousingService} from '../housing.service';
   imports: [HousingLocation],
   template: `
     <section>
-      <form>
-        <input type="text" placeholder="Filter by city" />
-        <button class="primary" type="button">Search</button>
+      <form (submit)="filterResults(filter.value); false">
+        <input type="text" placeholder="Filter by city" #filter />
+        <button class="primary" type="submit" >Search </button>
       </form>
     </section>
     <section class="results">
-      @for (housingLocation of housingLocationList; track $index) {
-      <app-housing-location [housingLocation]="housingLocation" />
-    }
+      @for (housingLocation of filteredLocationList; track $index) {
+       <app-housing-location [housingLocation]="housingLocation" />
+      }
     </section> `,
 
   styleUrls: ['./home.css']
 })
 export class Home {
    housingLocationList: HousingLocationInfo[] = [];
-  housingService: HousingService = inject(HousingService);
+   housingService: HousingService = inject(HousingService);
+   filteredLocationList: HousingLocationInfo[] = [];
+   
 
   constructor() {
-    this.housingLocationList = this.housingService.getAllHousingLocations();
-  }
+    this.housingLocationList = this.housingService.getAllHousingLocations();  
+    this.filteredLocationList = this.housingLocationList;
 }
+
+filterResults(text: string) {
+    
+    if (!text) {
+      this.filteredLocationList = this.housingLocationList;
+      return;
+    }
+
+      this.filteredLocationList = this.housingLocationList.filter(
+      (housingLocation: HousingLocationInfo)=>
+        housingLocation.city.toLowerCase().includes(text.toLowerCase())
+    );
+  }
+  }
+
+
